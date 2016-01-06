a96de74d8341c07ad65c987c1a1059fd7823ea78
Andrei Crudu <andrei.crudu@yahoo.com>
2016-01-06 17:31:02 +0200

    Add descriptions to the tests assertions

--- test/app/models/user_test.rb
+++ test/app/models/user_test.rb
@@ -14,11 +14,11 @@ class UserTest < Minitest::Test
 
   def test_all
     user = App::User.all.first
-    assert_kind_of App::User, user
-    assert_equal 1, user.id
+    assert_kind_of App::User, user, "The returned element should be of class App::User"
+    assert_equal 1, user.id, "The selected user has the correct id"
   end
 
   def test_table_name
-    assert_equal "users", App::User.table_name
+    assert_equal "users", App::User.table_name, "It can find the db table name based on the class name"
   end
 end
