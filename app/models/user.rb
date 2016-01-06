dd3aa4b72180c5fc11b1821a70f453505d610e8e
Andrei Crudu <andrei.crudu@yahoo.com>
2016-01-06 14:06:27 +0200

    Implement the id and name attributes for the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,17 @@
 module App
   class User
 
+    def initialize(attributes = {})
+      @attributes = attributes
+    end
+
+    def id
+      @attributes[:id]
+    end
+
+    def name
+      @attributes[:name]
+    end
+
   end
 end
